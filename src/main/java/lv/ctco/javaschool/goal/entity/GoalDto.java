996afbde8a643fa439752ae1647d52b498214195
Olga Polinkina <olyash@inbox.lv>
2018-08-23 13:17:07 +0300

    Test Calendar page (Delete latter!!!) js bug with max date - FIXED!

--- src/main/java/lv/ctco/javaschool/goal/entity/GoalDto.java
+++ src/main/java/lv/ctco/javaschool/goal/entity/GoalDto.java
@@ -1,13 +1,15 @@
 package lv.ctco.javaschool.goal.entity;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class GoalDto {
     private String username;
     private String goalMessage;
-    private LocalDateTime deadlineDate;
-    private LocalDateTime registeredDate;
+    private String deadlineDate;
+    private String registeredDate;
 
     public void setUsername(String username) {
         this.username = username;
@@ -25,19 +27,21 @@ public class GoalDto {
         return goalMessage;
     }
 
-    public void setDeadlineDate(LocalDateTime deadlineDate) {
-        this.deadlineDate = deadlineDate;
+    public void setDeadlineDate(LocalDate deadlineDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.mm.yyyy");
+        this.deadlineDate = deadlineDate.format(formatter);
     }
 
-    public LocalDateTime getDeadlineDate() {
+    public String getDeadlineDate() {
         return deadlineDate;
     }
 
     public void setRegisteredDate(LocalDateTime registeredDate) {
-        this.registeredDate = registeredDate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
+        this.registeredDate = registeredDate.format(formatter);
     }
 
-    public LocalDateTime getRegisteredDate() {
+    public String getRegisteredDate() {
         return registeredDate;
     }
 }
