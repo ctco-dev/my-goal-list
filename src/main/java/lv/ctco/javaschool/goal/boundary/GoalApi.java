30ba74f488449c28da234ae2e51ec85b8b3ac806
Olga Polinkina <olyash@inbox.lv>
2018-08-23 15:00:23 +0300

    #19 Added request to DB and autoFill result in drop downlist

--- src/main/java/lv/ctco/javaschool/goal/boundary/GoalApi.java
+++ src/main/java/lv/ctco/javaschool/goal/boundary/GoalApi.java
@@ -3,7 +3,7 @@ package lv.ctco.javaschool.goal.boundary;
 import lv.ctco.javaschool.auth.control.UserStore;
 import lv.ctco.javaschool.auth.entity.domain.User;
 import lv.ctco.javaschool.goal.control.GoalStore;
-
+import lv.ctco.javaschool.goal.entity.TagDto;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -13,6 +13,9 @@ import javax.ejb.Stateless;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Path("/goal")
 @Stateless
@@ -31,4 +34,32 @@ public class GoalApi {
     public void startPage() {
 //        User currentUser = userStore.getCurrentUser();
     }
+
+    @GET
+    @RolesAllowed({"ADMIN", "USER"})
+    @Path("/taglist")
+    public List<TagDto> drawRecordTable() {
+        List<TagDto> dto = new ArrayList<>();
+        int tagCount=new Random().nextInt(5)+3;
+        for(int i=0; i<tagCount; i++) {
+            dto.add(new TagDto(generateRandomWord()));
+        }
+        return dto;
+//        return goalStore.getTagList();
+//        List<TagDto> dto = goalStore.getTagList();
+//        return new Gson().toJson(dto);
+    }
+
+    private static String generateRandomWord() {
+        String randomStrings = new String("");
+        Random random = new Random();
+        char[] word = new char[random.nextInt(8) + 3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
+        for (int j = 0; j < word.length; j++) {
+            word[j] = (char) ('a' + random.nextInt(26));
+        }
+        randomStrings = new String(word);
+        return randomStrings;
+    }
+
+
 }
