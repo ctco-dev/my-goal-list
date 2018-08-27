2bedc52e30adcc1070fd2a49a8aa91420d6e2c65
Olga Polinkina <olyash@inbox.lv>
2018-08-27 11:47:18 +0300

    Changed TagDTO (added field cnt)

--- src/main/java/lv/ctco/javaschool/goal/entity/TagDto.java
+++ src/main/java/lv/ctco/javaschool/goal/entity/TagDto.java
@@ -2,16 +2,17 @@ package lv.ctco.javaschool.goal.entity;
 
 
 public class TagDto {
+    private String tagMessage;
+    private Long   cnt;
 
     public TagDto(){
-        this("");
+        this("", 0L);
     }
 
-    public TagDto(String newTag){
-        this.setTagMessage(newTag);
+    public TagDto(String tagMsg, Long cnt ){
+        this.tagMessage = tagMsg;
+        this.cnt = cnt;
     }
-
-    private String tagMessage;
 
     public void setTagMessage(String tagMessage) {
         this.tagMessage = tagMessage;
@@ -19,5 +20,13 @@ public class TagDto {
 
     public String getTagMessage() {
         return tagMessage;
+    }
+
+    public Long getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(Long cnt) {
+        this.cnt = cnt;
     }
 }
