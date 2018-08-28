ca73d76393db318a334d652e79e521658d150a69
Pavel Tihomirov <paveltihomirov92@gmail.com>
2018-08-28 14:48:07 +0300

    writing tests

--- src/test/java/lv/ctco/javaschool/auth/boundary/AuthenticationApiTest.java
+++ src/test/java/lv/ctco/javaschool/auth/boundary/AuthenticationApiTest.java
@@ -1,16 +1,10 @@
 package lv.ctco.javaschool.auth.boundary;
 
 import lv.ctco.javaschool.auth.control.UserStore;
-import lv.ctco.javaschool.auth.entity.domain.User;
-import lv.ctco.javaschool.auth.entity.dto.UserLoginDto;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class AuthenticationApiTest {
@@ -25,16 +19,16 @@ class AuthenticationApiTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    void testReturnUserDto() {
-        User user = new User();
-        user.setUsername("aa");
-        user.setPhone("1234567");
-        user.setEmail("a@b.com");
-        Mockito.when(userStore.getCurrentUser()).thenReturn(user);
-        UserLoginDto dto = authenticationApi.returnUserDto();
-        assertEquals(user.getUsername(), dto.getUsername());
-        assertEquals(user.getEmail(), dto.getEmail());
-        assertEquals(user.getPhone(), dto.getPhone());
-    }
+//    @Test
+//    void testReturnUserDto() {
+//        User user = new User();
+//        user.setUsername("aa");
+//        user.setPhone("1234567");
+//        user.setEmail("a@b.com");
+//        Mockito.when(userStore.getCurrentUser()).thenReturn(user);
+//        UserLoginDto dto = authenticationApi.returnUserDto();
+//        assertEquals(user.getUsername(), dto.getUsername());
+//        assertEquals(user.getEmail(), dto.getEmail());
+//        assertEquals(user.getPhone(), dto.getPhone());
+//    }
 }
--- src/test/java/lv/ctco/javaschool/goal/boundary/GoalApiTest.java
+++ src/test/java/lv/ctco/javaschool/goal/boundary/GoalApiTest.java
@@ -1,118 +1,91 @@
-//package lv.ctco.javaschool.goal.boundary;
-//
-//import lv.ctco.javaschool.auth.control.UserStore;
-//import lv.ctco.javaschool.auth.entity.domain.User;
-//import lv.ctco.javaschool.goal.control.GoalStore;
-//import lv.ctco.javaschool.goal.entity.Goal;
-//import lv.ctco.javaschool.goal.entity.GoalDto;
-//import lv.ctco.javaschool.goal.entity.Tag;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//
-//import javax.persistence.EntityManager;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.LocalTime;
-//import java.time.Month;
-//import java.time.format.DateTimeFormatter;
-//import java.util.*;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//
-//class GoalApiTest {
-////    @Test
-////    void createNewGoal() {
-////    }
-////    @Mock
-//    EntityManager em;
-//    @Mock
-//    private UserStore userStore;
-//    @Mock
-//    private GoalStore goalStore;
-//
-//    @InjectMocks
-//    private GoalApi goalApi;
-//    private Goal goal;
-//
-//    @BeforeEach
-//    void init() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    void getMyGoals() {
-//
-//        User user = new User();
-//        user.setUsername("aa");
-//
-//        goal = new Goal();
-//        goal.setGoalMessage("goal1");
-//        DateTimeFormatter dataFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-//        DateTimeFormatter dataTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
-//        goal.setRegisteredDate(LocalDateTime.parse("01.02.2018 10:41", dataTimeFormatter));
-//        goal.setDeadlineDate(LocalDate.parse("10.02.2018", dataFormatter));
-//        goal.setUser(user);
-//
-//        List<Goal> goalsList = new ArrayList<>();
-//        goalsList.add(goal);
-//
-//
-//        Mockito.when(userStore.getCurrentUser()).thenReturn(user);
-//        Mockito.when(goalStore.getGoalsListFor(user)).thenReturn(goalsList);
-//        List<GoalDto> dtos = goalApi.getMyGoals();
-//
-//        GoalDto dto = dtos.get(0);
-//        assertEquals(dto.getUsername(), "aa");
-//        assertEquals(dto.getGoalMessage(), "goal1");
-//        assertEquals(dto.getRegisteredDate(), "01.02.2018 10:41");
-//        assertEquals(dto.getDeadlineDate(), "10.02.2018");
-//    }
+package lv.ctco.javaschool.goal.boundary;
 
-//
-//    @Test
-//    @DisplayName("Check crate new Goal from dto")
-//    void testConvertDtoToGoal() {
-//        User user = new User();
-//        user.setUsername("aa");
-//        List<String> tagList = Arrays.asList("tag", "tag2", "tag3", "tag1", "tag2");
-//        Set<Tag> tagSet = new HashSet<Tag>(tagList);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
-//        LocalDateTime registeredDateTime = LocalDateTime.parse("10.01.2018 15:00", formatter);
-//        Goal goal = new Goal(user, "goal-mwessage", LocalDate.parse("10.02.2018"), registeredDateTime,tagList);
-//        GoalDto dto = new GoalDto();
-//
-//
-//
-//
-//        dto.setGoalMessage("goal-mwessage");
-//        dto.setDeadlineDate("10.02.2018");
-//        dto.setTagList(tagList);
-//        dto.setRegisteredDate("10.01.2018");
-////        LocalTime regTime = LocalTime.now();
-////        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
-////        String regTimeText = regTime.format(formatter);
-////        dto.setRegisteredDate(regTimeText);
-//
-////
-////        Mockito.when(userStore.getCurrentUser()).thenReturn(user);
-////
-////        assertEquals(goal, goalApi.convertDtoToGoal(dto));
-//////
-////
-////        assertEquals(user.getUsername(), "aa");
-////        assertEquals(goal.getGoalMessage(), "goal-mwessage");
-////
-////        regTime = LocalTime.now();
-////        formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");
-////        regTimeText = regTime.format(formatter);
-////        assertEquals(goal.getRegisteredDate(), regTimeText);
-////        assertEquals(goal.getDeadlineDate(), "10.02.2018");
-//    }
-//
-//}
+import lv.ctco.javaschool.auth.control.UserStore;
+import lv.ctco.javaschool.auth.entity.domain.User;
+import lv.ctco.javaschool.goal.control.GoalStore;
+import lv.ctco.javaschool.goal.entity.Goal;
+import lv.ctco.javaschool.goal.entity.GoalDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class GoalApiTest {
+    Goal goal = new Goal();
+    Goal goal2 = new Goal();
+    User user1 = new User();
+    User user2 = new User();
+    List<Goal> goalList1 = new ArrayList<>();
+    List<Goal> goalList2 = new ArrayList<>();
+    List<GoalDto> goalDtoList = new ArrayList<>();
+    @Mock
+    private UserStore userStore;
+    @Mock
+    private GoalStore goalStore;
+    @InjectMocks
+    private GoalApi goalApi;
+
+    @BeforeEach
+    void init() {
+        user1.setUsername("user");
+        user1.setEmail("user@user.com");
+        user1.setId((long) 1);
+        user1.setPassword("12345");
+        user1.setPhone("12345678");
+
+        user2.setUsername("admin");
+        user2.setEmail("admin@admin.com");
+        user2.setId((long) 1);
+        user2.setPassword("12345");
+        user2.setPhone("87654321");
+
+        goal.setUser(user1);
+        goal.setGoalMessage("abc");
+        goal.setRegisteredDate(LocalDateTime.now());
+        goal.setDeadlineDate(LocalDate.now().plusDays(1));
+        goal.setId((long) 1);
+        goal2.setUser(user2);
+        goal2.setGoalMessage("cde");
+    }
+
+    @Test
+    @DisplayName("Current user has No goals returns empty list of dto's")
+    void getMyGoals() {
+        when(userStore.getCurrentUser())
+                .thenReturn(user1);
+        when(goalStore.getGoalsListFor(user1))
+                .thenReturn(goalList1);
+
+        assertEquals(goalDtoList, goalApi.getMyGoals());
+    }
+
+    @Test
+    @DisplayName("Current user has goals returns list of dto's")
+    void getMyGoals2() {
+        goalList1.add(goal);
+        when(userStore.getCurrentUser())
+                .thenReturn(user1);
+        when(goalStore.getGoalsListFor(user1))
+                .thenReturn(goalList1);
+
+        assertEquals(goal.getUser().getUsername(), goalApi.getMyGoals().get(0).getUsername());
+        assertEquals(goal.getGoalMessage(), goalApi.getMyGoals().get(0).getGoalMessage());
+        assertEquals(goal.getId(), goalApi.getMyGoals().get(0).getId());
+        assertEquals(goal.getGoalMessage(), goalApi.getMyGoals().get(0).getGoalMessage());
+        assertEquals(goal.getGoalMessage(), goalApi.getMyGoals().get(0).getGoalMessage());
+    }
+
+}
